251b18d3625f1dc40222c64611098b752af33870
Mohammed Abbas <mohammadabbas50605@gmail.com>
2026-07-07 16:57:17 +0530

    Complete Category CRUD APIs and improve authentication

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_simplejwt',
 
     'rest_framework',
 
@@ -121,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
--- products/views.py
+++ products/views.py
@@ -1,9 +1,10 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework import status
 
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
 
 
 @api_view(['GET'])
@@ -32,6 +33,7 @@ def product_detail(request, id):
 
     return Response(serializer.data)
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def create_product(request):
 
     serializer = ProductSerializer(data=request.data)
@@ -44,6 +46,7 @@ def create_product(request):
 
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
 @api_view(['PUT'])
+@permission_classes([IsAuthenticated])
 def update_product(request, id):
 
     try:
@@ -63,6 +66,7 @@ def update_product(request, id):
 
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 @api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
 def delete_product(request, id):
 
     try:
@@ -79,4 +83,76 @@ def delete_product(request, id):
     return Response(
         {"message": "Product deleted successfully"},
         status=status.HTTP_200_OK
-    )        
+    )     
+@api_view(['GET'])
+def category_list(request):
+    categories = Category.objects.all()
+    serializer = CategorySerializer(categories, many=True)
+    return Response(serializer.data)
+@api_view(['GET'])
+def category_detail(request, id):
+
+    try:
+        category = Category.objects.get(id=id)
+
+    except Category.DoesNotExist:
+        return Response(
+            {"error": "Category not found"},
+            status=status.HTTP_404_NOT_FOUND
+        )
+    serializer = CategorySerializer(category)
+
+    return Response(serializer.data)
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def create_category(request):
+
+    serializer = CategorySerializer(data=request.data)
+
+    if serializer.is_valid():
+
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_category(request, id):
+
+    try:
+        category = Category.objects.get(id=id)
+
+    except Category.DoesNotExist:
+        return Response(
+            {"error": "Category not found"},
+            status=status.HTTP_404_NOT_FOUND
+        )
+
+    serializer = CategorySerializer(category, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def delete_category(request, id):
+
+    try:
+        category = Category.objects.get(id=id)
+
+    except Category.DoesNotExist:
+        return Response(
+            {"error": "Category not found"},
+            status=status.HTTP_404_NOT_FOUND
+        )
+
+    category.delete()
+
+    return Response(
+        {"message": "Category deleted successfully"},
+        status=status.HTTP_200_OK
+    )  
